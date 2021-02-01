9cfedf196e3b1f0391c90174e7cdba0a33c12e4d
wilsontbx <boonxian1993@hotmail.com>
2021-02-01 14:55:38 +0800

    added styling for todo logo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,12 @@
 import React from "react";
+import "./TodoItem.css";
 
 const TodoItem = ({ name, isDone }) => (
-  <div>
-    <span>
-      {isDone && (
-        <img
-          height="16"
-          alt="done"
-          src={`${process.env.PUBLIC_URL}/tick.png`}
-        />
-      )}
+  <div className="todo-item">
+    <span className="todo-item__completed">
+      {isDone && <img alt="done" src={`${process.env.PUBLIC_URL}/tick.png`} />}
     </span>
-    <span>{name}</span>
+    <span className="todo-item__name">{name}</span>
   </div>
 );
 
